31c651803b6a6ccbe8373b5cd8959494913d4410
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-06-15 20:52:19 -0700

    New swiftUI app

---
+++ DriveSafe/DriveSafe/DriveSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveSafeApp.swift
+//  DriveSafe
+//
+//  Created by Gurkirt Singh on 2025-06-15.
+//
+
+import SwiftUI
+
+@main
+struct DriveSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
