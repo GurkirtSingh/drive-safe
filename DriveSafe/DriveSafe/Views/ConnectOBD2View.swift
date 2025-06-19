efc8007090cff0d49d57853cca8622b8f77c69b2
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-06-18 18:23:28 -0700

    Modified View to initate async obd2 connection and display alert if there is error with error message.

--- DriveSafe/DriveSafe/Views/ConnectOBD2View.swift
+++ DriveSafe/DriveSafe/Views/ConnectOBD2View.swift
@@ -8,20 +8,50 @@
 import SwiftUI
 
 struct ConnectOBD2View: View {
-    @State var isConnected: Bool = false
+    
+    @State private var isConnecting: Bool = false
+    @State private var isAlertPresented: Bool = false
+    @State private var alertMessage: String = ""
+    
+    @StateObject private var obdManager: OBD2Client = OBD2Client.shared
+    
     var body: some View {
-        if !isConnected {
-            Button{
-                // Implement OB2 connection logic
-                print("Connecting OBD2")
-                
-            }label: {
-                Text("Connect OBD2")
-                    .padding()
-                    .foregroundStyle(Color.white)
-                    .background(Color.accentColor)
-                    .clipShape(Capsule())
+        if !obdManager.isConnected{
+            if isConnecting{
+                ProgressView("Connecting...")
+            } else{
+                VStack {
+                    Button{
+                        // Implement OB2 connection logic
+                        print("Connecting OBD2")
+                        isConnecting = true
+                        Task{
+                            do{
+                                try await obdManager.connect()
+                            } catch{
+                                isAlertPresented = true
+                                alertMessage = error.localizedDescription
+                                isConnecting = false
+                            }
+                        }
+                    } label: {
+                        Text("Connect OBD2")
+                            .padding()
+                            .foregroundStyle(Color.white)
+                            .background(Color.accentColor)
+                            .clipShape(Capsule())
+                    }
+                }.alert(isPresented: $isAlertPresented) {
+                    Alert(
+                        title: Text("Warning"),
+                        message: Text(alertMessage),
+                        dismissButton: .default(Text("Ok")))
+                }
             }
+            
+        } else{
+            Text("OBD2 is Connected!")
+                .foregroundStyle(.secondary)
         }
     }
 }
