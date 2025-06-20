d1378dcee9ad64c85451b9dd20f4fb0f35f4ec53
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-06-19 22:53:22 -0700

    Add TroubleCodeDetail view to display properties of TroubleCode model

--- DriveSafe/DriveSafe/Views/TroubleCodeDetail.swift
+++ DriveSafe/DriveSafe/Views/TroubleCodeDetail.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct TroubleCodeDetail: View {
+    var troubleCode: TroubleCode
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20){
+            Text(troubleCode.code)
+                .font(.largeTitle)
+                .bold()
+                .lineLimit(1)
+                .padding(.top, 50)
+            VStack{
+                switch troubleCode.status {
+                case .pending:
+                    Label("Pending", systemImage: "clock.arrow.circlepath")
+                        .foregroundColor(.orange)
+                case .confirmed:
+                    Label("Confirmed", systemImage: "checkmark.seal")
+                        .foregroundColor(.red)
+                case .permanent:
+                    Label("Permanent", systemImage: "exclamationmark.triangle")
+                        .foregroundColor(.gray)
+                }
+            }
+            Divider()
+            Text(troubleCode.description)
+                .font(.headline)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        .navigationTitle("Trouble Code Detail")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    TroubleCodeDetail()
+    TroubleCodeDetail(troubleCode: .init(code: "P0430", status: .permanent))
 }
