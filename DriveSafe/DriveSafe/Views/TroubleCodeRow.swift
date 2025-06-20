64401daa56a194f26e2afb1a6e3036c188c43b5b
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-06-19 22:56:29 -0700

    Modify TroubleCodeRow to display single trouble code

--- DriveSafe/DriveSafe/Views/TroubleCodeRow.swift
+++ DriveSafe/DriveSafe/Views/TroubleCodeRow.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct TroubleCodeRow: View {
+    var troubleCode: TroubleCode
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            VStack(alignment: .leading){
+                Text(troubleCode.code)
+                    .font(.headline)
+                Text(troubleCode.description)
+                    .font(.subheadline)
+                    .lineLimit(1)
+            }
+            
+            Spacer()
+            
+            switch troubleCode.status {
+            case .pending:
+                Label("Pending", systemImage: "clock.arrow.circlepath")
+                    .foregroundColor(.gray)
+            case .confirmed:
+                Label("Confirmed", systemImage: "checkmark.seal")
+                    .foregroundColor(.orange)
+            case .permanent:
+                Label("Permanent", systemImage: "exclamationmark.triangle")
+                    .foregroundColor(.red)
+            }
+        }
     }
 }
 
 #Preview {
-    TroubleCodeRow()
+    Group{
+        TroubleCodeRow(troubleCode: TroubleCode(code: "P0420", status: .pending))
+        TroubleCodeRow(troubleCode: TroubleCode(code: "P0420", status: .confirmed))
+        TroubleCodeRow(troubleCode: TroubleCode(code: "P0420", status: .permanent))
+    }
 }
